a7746ce755a87c28e937237dd16175afc34fb2ab
Yim Yoobin <yoobinbkk@naver.com>
2024-12-02 15:53:42 +0900

    init

---
+++ src/test/java/com/workshop/algorithm/AlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.algorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
